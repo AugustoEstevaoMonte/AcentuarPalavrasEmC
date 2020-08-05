de3165aafcf51df4981f3e3ec600d7771e0ad4ba
Augusto Estevão Monte <augusto-estevao@hotmail.com>
2020-08-04 21:07:51 -0300

    Update URI-Simple-calc.c

--- URI Online Judge/URI-Simple-calc.c
+++ URI Online Judge/URI-Simple-calc.c
@@ -3,14 +3,14 @@
 int main() {
  
 int codePiece1,numberOfPieces,codePiece2,numberOfPiece2;
-double value,value2,valorApagar;
+double value,value2,totalValue;
 scanf("%i",&codePiece1); 
 scanf("%i",&numberOfPieces); 
 scanf("%lf",&value); 
  scanf("%i",&codePiece2);
 scanf("%i",&numberOfPiece2);
 scanf("%lf",&value2);
-valorApagar=(value2*numberOfPiece2+value*numberOfPieces);
-printf("VALOR A PAGAR: R$ %0.2lf\n",valorApagar);
+totalValue=(value2*numberOfPiece2+value*numberOfPieces);
+printf("VALOR A PAGAR: R$ %0.2lf\n",totalValue);
     return 0;
 }
